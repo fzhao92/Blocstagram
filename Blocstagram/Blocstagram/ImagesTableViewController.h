a358e471ecab9f4758f973d3615e3cfb8730afbe
Forrest Zhao <fozhao92@gmail.com>
2016-03-10 23:22:52 -0800

    Completed: checkpoint 27

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Forrest Zhao on 3/9/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+@end
