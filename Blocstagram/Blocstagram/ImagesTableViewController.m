8889994dcd738296f7ce327485fa73cf3109e783
Forrest Zhao <fozhao92@gmail.com>
2016-03-11 22:00:36 -0800

    Completed: assignment 28

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -42,9 +42,8 @@
 #pragma mark - Table view data source
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [DataSource sharedInstance].mediaItems.count;
+    return [self items].count;
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     // #1
@@ -66,14 +65,14 @@
         [cell.contentView addSubview:imageView];
     }
     
-    Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
+    Media *item = [self items][indexPath.row];
     imageView.image = item.image;
     
     return cell;
 }
 
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
+    Media *item = [self items][indexPath.row];
     UIImage *image = item.image;
     return (CGRectGetWidth(self.view.frame)/image.size.width * image.size.height);
 }
@@ -98,7 +97,11 @@
     }   
 }
 
+#pragma mark - get media data
 
+-(NSArray *)items {
+    return [DataSource sharedInstance].mediaItems;
+}
 /*
 // Override to support rearranging the table view.
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
