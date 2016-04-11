354d044e4473b7c2d8ce2f3d84887bc9e20567f0
Forrest Zhao <fozhao92@gmail.com>
2016-04-10 18:33:24 -0700

    wip macbook

---
+++ Blocstagram/Blocstagram/LikeButton.m
@@ -0,0 +1,21 @@
+//
+//  LikeButton.m
+//  Blocstagram
+//
+//  Created by Forrest Zhao on 4/10/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import "LikeButton.h"
+
+@implementation LikeButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
