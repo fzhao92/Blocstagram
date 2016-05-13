44996d6ed5d8f39ff19227b2c72f6cc71c124fb4
Forrest Zhao <fozhao92@gmail.com>
2016-05-12 18:03:28 -0700

    move tapGesture code back into full screen vc

--- Blocstagram/Blocstagram/MediaFullScreenViewController.m
+++ Blocstagram/Blocstagram/MediaFullScreenViewController.m
@@ -9,11 +9,11 @@
 #import "MediaFullScreenViewController.h"
 #import "Media.h"
 
-@interface MediaFullScreenViewController () <UIScrollViewDelegate>
+@interface MediaFullScreenViewController () <UIScrollViewDelegate, UIGestureRecognizerDelegate>
 
 @property (nonatomic, strong) UITapGestureRecognizer *tap;
 @property (nonatomic, strong) UITapGestureRecognizer *doubleTap;
-@property (nonatomic, strong) UITapGestureRecognizer *tapOutsideView;
+@property (nonatomic, strong) UITapGestureRecognizer *tapOutsideRecognizer;
 
 @end
 
@@ -32,8 +32,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.tapOutsideView = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapOutsideDetected:)];
-    [self.view.window addGestureRecognizer:self.tapOutsideView];
+    if (!self.tapOutsideRecognizer) {
+        self.tapOutsideRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapBehind:)];
+        self.tapOutsideRecognizer.numberOfTapsRequired = 1;
+        self.tapOutsideRecognizer.cancelsTouchesInView = NO;
+        self.tapOutsideRecognizer.delegate = self;
+        [self.view.window addGestureRecognizer:self.tapOutsideRecognizer];
+    }
     
     // #1
     self.scrollView = [UIScrollView new];
@@ -152,16 +157,29 @@
     }
 }
 
-- (void)tapOutsideDetected:(UITapGestureRecognizer *)sender {
+- (IBAction)close:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)handleTapBehind:(UITapGestureRecognizer *)sender {
     if (sender.state == UIGestureRecognizerStateEnded)
     {
-        CGPoint location = [sender locationInView:self.view];
+        CGPoint location = [sender locationInView:nil]; //Passing nil gives us coordinates in the window
         
-        if (![self.view pointInside:location withEvent:nil]) {
-            [self.view.window removeGestureRecognizer:self.tapOutsideView];
-            [self dismissViewControllerAnimated:YES completion:nil];
+        //Then we convert the tap's location into the local view's coordinate system, and test to see if it's in or outside. If outside, dismiss the view.
+        
+        if (![self.view pointInside:[self.view convertPoint:location fromView:self.view.window] withEvent:nil])
+        {
+            // Remove the recognizer first so it's view.window is valid.
+            [self.view.window removeGestureRecognizer:sender];
+            [self close:sender];
         }
     }
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    return YES;
 }
 
 /*
